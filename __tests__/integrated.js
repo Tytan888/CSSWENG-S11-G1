8a3b57f008147aba91bef2581b47bb1f9559f032
rpineda26 <91407317+rpineda26@users.noreply.github.com>
2023-11-05 23:25:17 +0800

    cont: crud tests for projects, found id uniqueness bug

--- __tests__/integrated.js
+++ __tests__/integrated.js
@@ -45,14 +45,94 @@ describe("CRUD Project", () => {
         }
         await request.post('/add_project').attach('mainPhoto', project.mainPhoto).field("id", project.id).field("name", project.name).field("category", project.category).field("description", project.description).field("location", project.location).field("raisedDonations", project.raisedDonations).field("requiredBudget", project.requiredBudget).field("status", project.status).expect(200);
         const obj =  await db.findOne(Project, {name: "test project"});
-        expect(obj.name).toBeTruthy();
+        expect(obj.name).toEqual("test project");
+        expect(obj.category).toEqual("Education");
+        expect(obj.description).toEqual("test description");
+        expect(obj.location).toEqual("test location");
+        expect(obj.raisedDonations).toEqual(0);
+        expect(obj.requiredBudget).toEqual(1000);
+        expect(obj.status).toEqual("Ongoing");
         return obj;
     });
 
-    test.todo('should update project in the database');
-    test.todo('should delete project in the database');
-    test.todo('should get project in the database');
-    test.todo('should have unique id');
+    test('should update project in the database', async () => {
+        const project = {
+            id: 1,
+            name: "test project",
+            category: "Education",
+            description:"test description",
+            location: "test location",
+            raisedDonations: 0,
+            requiredBudget: 1000,
+            status: "Ongoing",
+            mainPhoto : "__tests__/test_assets/image_asset.png",     
+        }
+        await request.post('/add_project').attach('mainPhoto', project.mainPhoto).field("id", project.id).field("name", project.name).field("category", project.category).field("description", project.description).field("location", project.location).field("raisedDonations", project.raisedDonations).field("requiredBudget", project.requiredBudget).field("status", project.status).expect(200);
+        var obj =  await db.findOne(Project, {name: "test project"});
+        const mainPhoto = obj.mainPhoto;
+        await request.put('/edit_project').attach('mainPhoto', project.mainPhoto).field("id", "Project-0000001").field("name","test update name").field("category", "Health").field("description", "test update description").field("location", "test update location").field("raisedDonations", 50).field("requiredBudget", 5000).field("status", "Past").expect(200);
+        var newobj =  await db.findOne(Project, {name: "test project"});
+        expect(newobj).toBeNull();
+        obj =  await db.findOne(Project, {name: "test update name"});
+        expect(obj.name).toEqual("test update name");
+        expect(obj.category).toEqual("Health");
+        expect(obj.description).toEqual("test update description");
+        expect(obj.location).toEqual("test update location");
+        expect(obj.raisedDonations).toEqual(50);
+        expect(obj.requiredBudget).toEqual(5000);
+        expect(obj.status).toEqual("Past");
+        expect(obj.mainPhoto).not.toEqual(mainPhoto);
+        const img = await request.get('/imageByName').set("name", mainPhoto).expect(404);
+        return obj;
+    });
+    test('should delete project in the database', async () => {
+        const project = {
+            id: 1,
+            name: "test project",
+            category: "Education",
+            description:"test description",
+            location: "test location",
+            raisedDonations: 0,
+            requiredBudget: 1000,
+            status: "Ongoing",
+            mainPhoto : "__tests__/test_assets/image_asset.png",     
+        }
+        await request.post('/add_project').attach('mainPhoto', project.mainPhoto).field("id", project.id).field("name", project.name).field("category", project.category).field("description", project.description).field("location", project.location).field("raisedDonations", project.raisedDonations).field("requiredBudget", project.requiredBudget).field("status", project.status).expect(200);
+        const obj =  await db.findOne(Project, {name: "test project"});
+        const mainPhoto = obj.mainPhoto;
+        console.log("deleteid "+obj.id);
+        await request.delete('/delete_project').send({id: obj.id});
+        const newobj =  await db.findOne(Project, {name: "test project"});
+        expect(newobj).toBeNull();
+        const img = await request.get('/imageByName').set("name", mainPhoto).expect(404);
+        return obj;
+    });
+    test('should have unique id', async() =>{
+        const project = {
+            id: 1,
+            name: "test project",
+            category: "Education",
+            description:"test description",
+            location: "test location",
+            raisedDonations: 0,
+            requiredBudget: 1000,
+            status: "Ongoing",
+            mainPhoto : "__tests__/test_assets/image_asset.png",     
+        }
+        await request.post('/add_project').attach('mainPhoto', project.mainPhoto).field("id", project.id).field("name", project.name).field("category", project.category).field("description", project.description).field("location", project.location).field("raisedDonations", project.raisedDonations).field("requiredBudget", project.requiredBudget).field("status", project.status).expect(200);
+        const obj =  await db.findOne(Project, {name: "test project"});
+        await request.post('/add_project').attach('mainPhoto', project.mainPhoto).field("id", project.id).field("name", project.name).field("category", project.category).field("description", project.description).field("location", project.location).field("raisedDonations", project.raisedDonations).field("requiredBudget", project.requiredBudget).field("status", project.status).expect(200);
+        const obj2 =  await db.findOne(Project, {name: "test project"});
+        await request.post('/add_project').attach('mainPhoto', project.mainPhoto).field("id", project.id).field("name", project.name).field("category", project.category).field("description", project.description).field("location", project.location).field("raisedDonations", project.raisedDonations).field("requiredBudget", project.requiredBudget).field("status", project.status).expect(200);
+        const obj3 =  await db.findOne(Project, {name: "test project"});
+        console.log("obj1 "+obj.id+" obj2 "+obj2.id+" obj3 "+obj3.id);
+
+        expect(obj.id).not.toEqual(obj2.id);
+        expect(obj.id).not.toEqual(obj3.id);
+        expect(obj2.id).not.toEqual(obj3.id);
+        return obj;
+        
+    });
     test.todo('should not be able to send with non image file upload');
 });
 
